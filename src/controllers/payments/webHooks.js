32f4db9c9fda81fb877a64d4136e1d70db34eb32
Joel <joeljuliandurand@gmail.com>
2022-11-12 02:35:11 -0300

    webHook2

--- src/controllers/payments/webHooks.js
+++ src/controllers/payments/webHooks.js
@@ -1,21 +1,41 @@
-// import fs from "fs"
-// import Sales from "../../models/Sales.js";
-// // import mercadopago from 'mercadopago'
+// import User from "../../models/User"
+
+// {
+//     action: 'test.created',
+//     api_version: 'v1',
+//     application_id: '6604225923180824',
+//     date_created: '2021-01-01 02:02:02 +0000 UTC',
+//     id: '123456',
+//     live_mode: 'false',
+//     type: 'test',
+//     user_id: 429637065,
+//     data: { id: '123456789' }
+//     }
 
 export const webHooks = async (req, res, next) => {
     try {
 
-        console.log(req.body)
-//         let data = req.body
+        const { data } = req.body
 
-//         if (data['action'] == 'payment.created') {
-//             fs.writeFileSync(`${__dirname}/../webHookResponses.json`, JSON.stringify(data))
-//         }
+        const compra = await axios.get(`https://api.mercadopago.com/v1/payments/${data.id}`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.ACCESS_TOKEN_PRUE_MP}`
+            }
+        })
+        console.log(compra.data)
 
+        // if(compra.data.status === "approved" && compra.data.status_detail === "accredited") {
+        //     const user = await User.findByIdAndUpdate({_id: compra.data.items.id}, {
+        //         verificationPay: true
+        //     })
+        // }
+        
         res.status(200).send('ok')
     } catch (error) {
         console.log(error)
         next()
     }
+
 
 }
