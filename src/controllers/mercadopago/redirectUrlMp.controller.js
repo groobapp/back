014eb5da2ae305b0c6b01235800370a7c0a3f733
Joel <joeljuliandurand@gmail.com>
2022-11-16 14:56:38 -0300

    mp connect2

--- src/controllers/mercadopago/redirectUrlMp.controller.js
+++ src/controllers/mercadopago/redirectUrlMp.controller.js
@@ -2,9 +2,19 @@ import User from "../../models/User.js"
 import axios from "axios"
 
 export const redirectUrlMp = async (req, res, next) => {
-    const { code } = req.query
+    const { code, state } = req.query
 
-    res.status(200).send(code)
+    const generateAccessToken = await axios.post(`https://api.mercadopago.com/oauth/token`, {
+        client_id: process.env.CLIENT_ID_MP,
+        client_secret: process.env.CLIENT_SECRET_MP,
+        code: code,
+        grant_type: "authorization_code",
 
-
+    }, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.ACCESS_TOKEN_PROD_MP}`
+        }
+    })
+    res.status(200).send(generateAccessToken)
 }
