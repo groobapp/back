f357eda50283e2fd0a37b6466c386c582d849c32
Joel <joeljuliandurand@gmail.com>
2022-11-16 13:49:49 -0300

    asd

--- src/controllers/mercadopago/redirectUrlMp.controller.js
+++ src/controllers/mercadopago/redirectUrlMp.controller.js
@@ -2,10 +2,9 @@ import User from "../../models/User.js"
 import axios from "axios"
 
 export const redirectUrlMp = async (req, res, next) => {
-    console.log(req.body)
-    console.log(req.query)
-    console.log(req.params)
-    res.status(200).send("Recibí algo")
+    const { code } = req.query
+
+    res.status(200).send(code)
 
 
 }
