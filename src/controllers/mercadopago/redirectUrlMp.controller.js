37773a588816f368885a575ddb687841536f4cf9
Joel <joeljuliandurand@gmail.com>
2022-11-16 15:23:34 -0300

    mp connect 3

--- src/controllers/mercadopago/redirectUrlMp.controller.js
+++ src/controllers/mercadopago/redirectUrlMp.controller.js
@@ -4,17 +4,25 @@ import axios from "axios"
 export const redirectUrlMp = async (req, res, next) => {
     const { code, state } = req.query
 
-    const generateAccessToken = await axios.post(`https://api.mercadopago.com/oauth/token`, {
-        client_id: process.env.CLIENT_ID_MP,
-        client_secret: process.env.CLIENT_SECRET_MP,
-        code: code,
-        grant_type: "authorization_code",
+    try {
+        const result = await axios.post(`https://api.mercadopago.com/oauth/token`, {
+            client_secret: process.env.CLIENT_SECRET_MP,
+            client_id: process.env.CLIENT_ID_MP,
+            grant_type: "authorization_code",
+            code: code,
 
-    }, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.ACCESS_TOKEN_PROD_MP}`
-        }
-    })
-    res.status(200).send(generateAccessToken)
+        }, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${process.env.ACCESS_TOKEN_PRUE_MP}`
+            }
+        })
+        console.log(result)
+        res.status(200).json("veamos q tul")
+
+    } catch (error) {
+        console.log(error)
+        res.json(error)
+        next()
+    }
 }
