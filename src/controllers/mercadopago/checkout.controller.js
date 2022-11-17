ee7445fa45860b85e23faea5d3c975d8c2b016a2
Joel <joeljuliandurand@gmail.com>
2022-11-17 18:48:49 -0300

    fix import

--- src/controllers/mercadopago/checkout.controller.js
+++ src/controllers/mercadopago/checkout.controller.js
@@ -1,5 +1,5 @@
 import mercadopago from 'mercadopago'
-import User from '../../models/User'
+import User from '../../models/User.js'
 
 export const usersProductsMP = async (req, res) => {
   const { 
