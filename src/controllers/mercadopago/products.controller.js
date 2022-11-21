01d64d2e35b9ef9a8dcf2fa81121d0243f6f621e
Joel <joeljuliandurand@gmail.com>
2022-11-20 22:16:21 -0300

    posts with price by user id

--- src/controllers/mercadopago/products.controller.js
+++ src/controllers/mercadopago/products.controller.js
@@ -1,12 +1,12 @@
 import User from '../../models/User.js'
 import Publication from '../../models/Publication.js'
 
-export const postsWithPriceByUserId = async (req, res, next) => {
+export const postsWithPriceByUser = async (req, res, next) => {
     try {
         const user = await User.findById(req.userId)
         const publications = user?.publications
 
-        
+
         const posts = await Publication.find({
             _id: {
                 $in: publications
@@ -14,7 +14,35 @@ export const postsWithPriceByUserId = async (req, res, next) => {
         })
 
         const postsWithPrice = posts.filter(post => {
-            if(post.price > 0) {
+            if (post.price > 0) {
+                return post
+            }
+        })
+        res.status(200).json(postsWithPrice)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('An internal server error occurred');
+        next()
+    }
+}
+
+
+
+export const postsWithPriceByUserId = async (req, res, next) => {
+    try {
+        const { userId } = req.body
+        const user = await User.findById({ _id: userId })
+        const publications = user?.publications
+
+
+        const posts = await Publication.find({
+            _id: {
+                $in: publications
+            }
+        })
+
+        const postsWithPrice = posts.filter(post => {
+            if (post.price > 0) {
                 return post
             }
         })
--- src/routes/mercadopago.routes.js
+++ src/routes/mercadopago.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { usersProductsMP } from '../controllers/mercadopago/checkout.controller.js'
 import { verifyAccountPay } from '../controllers/mercadopago/verification.controller.js'
-import { postsWithPriceByUserId } from "../controllers/mercadopago/products.controller.js"
+import { postsWithPriceByUser, postsWithPriceByUserId } from "../controllers/mercadopago/products.controller.js"
 import { webHooks } from '../controllers/mercadopago/webHooks.js'
 import { redirectUrlMp } from '../controllers/mercadopago/redirectUrlMp.controller.js'
 import { TokenValidator } from '../libs/tokenValidator.js';
@@ -13,7 +13,8 @@ router.post('/preferenceVerification', TokenValidator, verifyAccountPay)
 router.post('/preferenceProduct', TokenValidator, usersProductsMP)
 router.post('/notifications', webHooks)
 
-router.get('/productsByUser', TokenValidator, postsWithPriceByUserId)
+router.get('/productsByUser', TokenValidator, postsWithPriceByUser)
+router.get('/productsByUser/:id', TokenValidator, postsWithPriceByUserId)
 router.get('/mp-connect', redirectUrlMp)
 
 
