5819905f7c68a6aa0e0acd0b5d724a4b65d09c86
Joel <joeljuliandurand@gmail.com>
2022-11-20 23:22:30 -0300

    fix search users

--- src/controllers/interaction/searchUser.controller.js
+++ src/controllers/interaction/searchUser.controller.js
@@ -2,19 +2,17 @@ import User from '../../models/User.js'
 
 export const searchUser = async (req, res, next) => {
     try {
-
-        // Recibir mi id por params o enviar token en cada consulta
+        // const myUser = await User.findById(req.userId)
         const { input } = req.query
-        console.log(input)
         if (input === undefined || input === null || input === "") return
         else {
             let users = await User.find()
             const result = users.filter(user => {
-                if (user.userName.toLowerCase().includes(input)
-                    || user.description.toLowerCase().includes(input)
-                    || user.firstName.toLowerCase().includes(input)
-                    || user.lastName.toLowerCase().includes(input)
-                    || user.email.toLowerCase().includes(input)
+                if (user.userName?.toLowerCase().includes(input)
+                    || user.description?.toLowerCase().includes(input)
+                    || user.firstName?.toLowerCase().includes(input)
+                    || user.lastName?.toLowerCase().includes(input)
+                    || user.email?.toLowerCase().includes(input)
                 ) {
                     return user
                 }
@@ -26,7 +24,6 @@ export const searchUser = async (req, res, next) => {
             // una vez realizada la búsqueda
             // el resultado de usuarios compararlo con mi lista de seguidos
             // ordenar y poner arriba primero a los que coincidan con esa lista
-            res.status(200).json(result)
             return res.status(200).json(result)
         }
     } catch (error) {
