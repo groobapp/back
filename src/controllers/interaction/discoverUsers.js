917c9b231462b3d36e914675db6cea752e03a8eb
Joel <joeljuliandurand@gmail.com>
2022-12-16 01:15:37 -0300

    fix discover

--- src/controllers/interaction/discoverUsers.js
+++ src/controllers/interaction/discoverUsers.js
@@ -7,25 +7,33 @@ export const discoverUsers = async (req, res, next) => {
         const user = await User.findById(req.userId)
         const allPublications = await Publication.find()
         if (user.explicitContent === true) {
-            const filterByPhoto = allPublications.filter(post => {
-                if (post.images.length > 0) {
+            const filterByExplicitContentAndImages = allPublications.filter(post => {
+                if (post => post.explicitContent === false
+                    || post.explicitContent === true
+                    && post.images.length > 0
+                    && post.price === 0) {
                     return post;
+                }
+            })
+            const orderByDate = filterByExplicitContentAndImages.sort((a, b) => {
+                if (a.createdAt < b.createdAt) return 1;
+                return -1;
+            })
+            res.status(200).json({ orderByDate, mpAccountAsociated: user?.mpAccountAsociated })
+
+        } else if (user.explicitContent === false) {
+            const filterByPhoto = allPublications.filter(post => {
+                if (post.explicitContent === false
+                    && post.images.length > 0
+                    && post.price === 0) {
+                    return post
                 }
             })
             const orderByDate = filterByPhoto.sort((a, b) => {
                 if (a.createdAt < b.createdAt) return 1;
                 return -1;
             })
-            res.status(200).json(orderByDate)
-
-        } else {
-            const filterByExplicitContentAndImages = allPublications.filter
-                (post => post.explicitContent === false && post.images.length > 0)
-            const orderByDate = filterByExplicitContentAndImages.sort((a, b) => {
-                if (a.createdAt < b.createdAt) return 1;
-                return -1;
-            })
-            res.status(200).json({orderByDate, mpAccountAsociated: user?.mpAccountAsociated})
+            res.status(200).json({ orderByDate, mpAccountAsociated: user?.mpAccountAsociated })
         }
         return closeConnectionInMongoose;
     } catch (error) {
