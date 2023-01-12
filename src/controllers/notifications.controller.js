62590594f46ea9598ab4e671860946c44e3237bb
Joel <joeljuliandurand@gmail.com>
2023-01-12 18:44:39 -0300

    deploy

--- src/controllers/notifications.controller.js
+++ src/controllers/notifications.controller.js
@@ -1,5 +1,5 @@
-import Publication from "../models/Publication"
-import User from "../models/User"
+import Publication from "../models/Publication.js"
+import User from "../models/User.js"
 
 export const notification = async (req, res, next) => {
     try {
