51491157b757feb290fc3562996d4a97c81f1b6f
joedurand-hub <joeljuliandurand@gmail.com>
2024-01-11 06:13:23 -0300

    cambios en getAllPostsByFollowings

--- src/controllers/interaction/getAllPostsByFollowings.controller.js
+++ src/controllers/interaction/getAllPostsByFollowings.controller.js
@@ -82,35 +82,31 @@ export const getAllPostsByFollowings = async (req, res, next) => {
             res.status(500).json("Usuario no loggeado")
             return
         }
-        let myPosts = myUser.publications.map((id) => id)
+        const myPosts = myUser.publications.map((id) => id)
         const postsByMyUser = await Publication.find({
             _id: {
                 $in: myPosts
             }
         })
-        console.log("MIS POSTS: ", postsByMyUser)
 
-        let allMyIds = myUser.followings.map((id) => id)
-
-        console.log("A QUIENES SIGO: ", allMyIds)
-        if (allMyIds.length === 0) return res.status(200).json(myPosts)
-
+        const allMyIds = myUser.followings.map((id) => id)
         const postsByFollowings = await Publication.find({
             userIdCreatorPost: {
                 $in: allMyIds
             }
         })
 
-        console.log("posts de quienes sigo: ", postsByFollowings)
-        const allPosts = postsByMyUser.concat(postsByFollowings)
-
-        const uniqueIds = [...new Set(allPosts.map(post => post._id.toString()))];
-        const noDuplicates = uniqueIds.map(id => allPosts.find(post => post._id.toString() === id));
-
+        if (postsByFollowings.length > 0) {
+            const allPosts = postsByMyUser.concat(postsByFollowings)
+            const uniqueIds = [...new Set(allPosts.map(post => post._id.toString()))];
+            const noDuplicates = uniqueIds.map(id => allPosts.find(post => post._id.toString() === id));
+            const data = noDuplicates.sort((a, b) => b.createdAt - a.createdAt);
+            res.status(200).json(data)
+        }
+        const uniqueIds = [...new Set(myPosts.map(post => post._id.toString()))];
+        const noDuplicates = uniqueIds.map(id => myPosts.find(post => post._id.toString() === id));
         const data = noDuplicates.sort((a, b) => b.createdAt - a.createdAt);
-
         res.status(200).json(data)
-
 
     } catch (error) {
         console.log(error)
