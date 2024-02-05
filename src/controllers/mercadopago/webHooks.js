a9b3823aa855507c8b7f2005ec090f0c1f3a6978
joedurand-hub <joeljuliandurand@gmail.com>
2024-02-05 16:00:19 -0300

    asddsa

--- src/controllers/mercadopago/webHooks.js
+++ src/controllers/mercadopago/webHooks.js
@@ -4,21 +4,19 @@ import Wallet from "../../models/Wallet.js"
 
 export const webHooks = async (req, res, next) => {
     const { type, data } = req.body
-    console.log("BODY", req.body)
     try {
-        const compra = await axios.get(`https://api.mercadopago.com/v1/payments/${data.id}`, {
+        const compra = await axios.get(`https://api.mercadopago.com/v1/payments/${data?.id}`, {
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${process.env.ACCESS_TOKEN_PROD_MP}`
             }
         })
-        console.log("COMPRA:", compra)
         if (type === "payment" &&
             compra.data.status === "approved" &&
             compra.data.status_detail === "accredited" &&
             compra.data.metadata.coins_quantity && compra.data.metadata.user_buyer) {
             const wallet = await Wallet.findById({ _id: compra.data.metadata.user_buyer })
-            if (wallet === undefined || wallet === null) {
+            if (!wallet) {
                 res.status(400).json("No se ha encontrado una billetera")
             }
             wallet.balance = compra.data.metadata.coins_quantity
