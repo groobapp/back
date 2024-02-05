708fe3b7c1e837b3cb3c960a56415a02833cf9e0
joedurand-hub <joeljuliandurand@gmail.com>
2024-02-05 00:21:29 -0300

    asd

--- src/controllers/mercadopago/webHooks.js
+++ src/controllers/mercadopago/webHooks.js
@@ -7,28 +7,28 @@ export const webHooks = async (req, res, next) => {
     console.log(type, data)
     try {
 
-        const compra = await axios.get(`https://api.mercadopago.com/v1/payments/${data?.userBuyer}`, {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${process.env.ACCESS_TOKEN_PROD_MP}`
-            }
-        })
-        if (type === "payment" &&
-            compra.data.status === "approved" &&
-            compra.data.status_detail === "accredited" &&
-            compra.data.metadata.coinsQuantity && compra.data.metadata.userBuyer) {
-            const wallet = await Wallet.findById({ _id: compra.data.metadata.userBuyer })
-            if (wallet === undefined || wallet === null) {
-                res.status(400).json("No se ha encontrado una billetera")
-            }
-            wallet.balance = compra.data.metadata.coinsQuantity
-            wallet.historyPurchases = wallet.historyPurchases.push({
-                price: compra.data.metadata.price,
-                amount: compra.data.metadata.coinsQuantity,
-                date: new Date()
-            })
-            await wallet.save()
-        }
+        // const compra = await axios.get(`https://api.mercadopago.com/v1/payments/${data?.userBuyer}`, {
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         "Authorization": `Bearer ${process.env.ACCESS_TOKEN_PROD_MP}`
+        //     }
+        // })
+        // if (type === "payment" &&
+        //     compra.data.status === "approved" &&
+        //     compra.data.status_detail === "accredited" &&
+        //     compra.data.metadata.coinsQuantity && compra.data.metadata.userBuyer) {
+        //     const wallet = await Wallet.findById({ _id: compra.data.metadata.userBuyer })
+        //     if (wallet === undefined || wallet === null) {
+        //         res.status(400).json("No se ha encontrado una billetera")
+        //     }
+        //     wallet.balance = compra.data.metadata.coinsQuantity
+        //     wallet.historyPurchases = wallet.historyPurchases.push({
+        //         price: compra.data.metadata.price,
+        //         amount: compra.data.metadata.coinsQuantity,
+        //         date: new Date()
+        //     })
+        //     await wallet.save()
+        // }
         res.status(200).send('ok')
 
     } catch (error) {
