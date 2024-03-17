6c12d79700499d70c6b14c1d255c232e8628d47e
joedurand-hub <joeljuliandurand@gmail.com>
2024-03-16 23:56:55 -0300

    asd dsa

--- src/controllers/interaction/discoverUsers.js
+++ src/controllers/interaction/discoverUsers.js
@@ -3,21 +3,17 @@ import User from '../../models/User.js'
 
 let offset = 0; // Variable para llevar el seguimiento del índice de inicio de la próxima página
 const pageSize = 20; // Tamaño de la página
+let allProfiles = []; // Variable para almacenar todos los perfiles
 
 export const discoverUsers = async (req, res, next) => {
     try {
-        // Obtener la cantidad total de perfiles
-        const totalCount = await User.countDocuments();
-        if (totalCount === 0) {
-            return res.status(404).json({ message: 'No hay usuarios disponibles.' });
-        }
-
-        // Verificar si se ha alcanzado el límite de usuarios
-        if (offset >= totalCount) {
-            return res.status(200).json([]); // No hay más usuarios para enviar
+        if (allProfiles.length === 0 || offset >= allProfiles.length) {
+            // Si no hay perfiles cargados o se han enviado todos, cargar nuevamente todos los perfiles
+            allProfiles = await User.find().limit(500);
+            offset = 0; // Restablecer el offset a cero
         }
         // Obtener el siguiente lote de usuarios
-        const randomUsers = await User.find().skip(offset).limit(pageSize);
+        const randomUsers = allProfiles.slice(offset, offset + pageSize);
         offset += pageSize; // Incrementar el offset para la próxima solicitud
         res.status(200).json(randomUsers);
     } catch (error) {
@@ -26,6 +22,7 @@ export const discoverUsers = async (req, res, next) => {
         next(error);
     }
 };
+
 export const discoverPostsWithImages = async (req, res, next) => {
     try {
         if (!req.userId) {
